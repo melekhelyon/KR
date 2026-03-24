a4c5d9609dc98abe7fc5d726f04232df489230bb
melekhelyon <melekhelyon@gmail.com>
2026-03-24 18:56:09 +0800

    0-50 не проверено

---
+++ fin/database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('nickname')->unique();
+            $table->string('password');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('phone', 20)->nullable();
+            $table->char('default_currency', 3)->default('RUB');
+            $table->boolean('is_verified')->default(false);
+            $table->timestamps();
+            
+            $table->index('nickname');
+            $table->index('email');
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ fin/resources/js/app.js
@@ -0,0 +1,7 @@
+require('./bootstrap');
+
+import { createApp } from 'vue';
+
+import App from './app.vue';
+
+createApp(App).mount("#app");
